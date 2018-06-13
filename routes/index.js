977d68f1291dd19cfc2874c5a36c9ce4517e4351
ElliotSimpson <simpsonj@uncw.edu>
2018-06-13 15:30:01 -0400

    updated makerspace url

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  request('https://makerspace.uncw.edu/api/v1/queue', { json: true }, (err, httpres, body) => {
+  request('https://digitalmakerspace.uncw.edu/api/v1/queue', { json: true }, (err, httpres, body) => {
     if (err) { res.send('Error.') }
     res.render('index', { queue: body.data });
   });
