60fa85f339ae094b428cbafed10a82a7348b6311
ElliotSimpson <simpsonj@uncw.edu>
2018-06-21 09:04:20 -0400

    change status names on frontend

--- app.js
+++ app.js
@@ -44,10 +44,25 @@ Handlebars.registerHelper('getRowColor', function(status) {
   switch (status) {
     case 'Just Arrived':
       return 'danger'
+    case 'Reviewed and Approved':
+      return 'info'
     case 'Started Printing':
       return 'warning'
     case 'Ready for Pickup':
       return 'active'
+  }
+});
+
+Handlebars.registerHelper('changeStatusName', function(status) {
+  switch (status) {
+    case 'Just Arrived':
+      return 'Submitted'
+    case 'Reviewed and Approved':
+      return 'Reviewed and Approved'
+    case 'Started Printing':
+      return 'Printing'
+    case 'Ready for Pickup':
+      return 'Ready for Pickup'
   }
 });
 
